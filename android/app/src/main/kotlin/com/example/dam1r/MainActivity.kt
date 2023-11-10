0e6aa4e5f4e6ca3a3f84b625e401825dec1385a9
Student <student@sp4_zam_uvr.first.local>
2023-11-10 16:08:58 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dam1r/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dam1r
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    color: Colors.white,
+    child: Image.asset('assets/In no time-pana 1.png'),
+  ),
+  );
+}
